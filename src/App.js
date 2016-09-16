36ab2691fe2d1eab3a4e2b6453c4fb01724d6833
oliverlangmo <oliver.langmo@gmail.com>
2016-09-15 23:04:50 -0500

    added song

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Song,
+  Sequencer,
+  Synth,
+} from '../node_modules/react-music/lib/index.js'
 
 class App extends Component {
   render() {
@@ -10,9 +15,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Song
+          playing={this.state.playing}
+          tempo={120}>
+          <Sequencer
+                        resolution={16}
+                        bars={2}
+                      >
+                        <Synth
+                          type="sine"
+                          steps={[
+                            [0, 8, 'c2'],
+                            [8, 4, 'c2'],
+                            [12, 4, 'd#2'],
+                            [16, 8, 'f2'],
+                            [24, 8, 'f1'],
+                          ]}
+                        />
+                      </Sequencer>
+        </Song>
       </div>
     );
   }
